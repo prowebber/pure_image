e42cab3b1b54cbba6a52515c80c58071db7af9c7
Steven Holdaway <webpurellc@gmail.com>
2020-05-30 11:57:23 -0400

    feat: Composer test

---
+++ src/Main.php
@@ -0,0 +1,25 @@
+<?php
+namespace pure_image;
+
+class Main {
+	
+	//---------------	Class-Wide Variables	-------------
+	//---------------	Injected Classes	-----------------
+	//---------------	Added Classes		-----------------
+	
+	
+	/**
+	 * Main Constructor
+	 */
+	public function __construct(){
+		
+	}
+	
+	/**
+	 * Main Controller Function
+	 */
+	public function classConfig(){
+		echo "Finally got it working...I think";
+	}
+}
+?>
---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+// Composer autoload
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$class = new pure_image\Main();
+$class->classConfig();
