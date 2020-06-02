e9d4c4cf1545360b590593f7384176529e300550
Steven Holdaway <webpurellc@gmail.com>
2020-06-02 08:13:52 -0400

    test: Add result array

--- tests/test.php
+++ tests/test.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $img_dir = __DIR__ . '/img_source';
 $img_name = '\water-original.jpg';
 
-$pimage = new PureImage();                      # Init Pure Image
+$pimage = new \pure_image\PureImage();          # Init Pure Image
 $pimage->setMaxImageSize(10000000);             # Set the max allowed image size (for the source image)
 $pimage->add->image($img_dir . $img_name);      # Specify the source image
 
@@ -80,6 +80,9 @@ $pimage->save->image();             # Save all the images
 if(!$pimage->isErrorFree()){        # If errors exist
 	echo '<h1>Errors Exist</h1>';
 }
+
+
+$results = $pimage->getResult();
 
 
 $pimage->showErrors();              # Print any error message
