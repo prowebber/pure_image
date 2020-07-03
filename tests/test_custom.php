460b099a032d3c305cf00902f319375bd5c3d0ef
Steven Holdaway <webpurellc@gmail.com>
2020-07-03 13:19:49 -0400

    refactor: Cleanup

--- tests/test_custom.php
+++ tests/test_custom.php
@@ -4,16 +4,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $img_dir = __DIR__ . '/img_source';
-//$img_name = '\7674.jpg';
-//$img_name = '\water-original.jpg';
+$img_name = '\water-original.jpg';
 //$img_name = '\water-original-flipped.jpg';
 //$img_name = '\water-original-unflipped.jpg';
 
 
 # Person Test
-//$img_name = '\person_test.jpg';
-$img_name = '\person_test_horiz_flipped.jpg';
-
 $pimage = new \pure_image\PureImage();          # Init Pure Image
 $pimage->setMaxImageSize(10000000);             # Set the max allowed image size (for the source image)
 $pimage->add->image($img_dir . $img_name);      # Specify the source image
@@ -30,18 +26,20 @@ $pimage->add->image($img_dir . $img_name);      # Specify the source image
 //	'save_path' => $img_dir . '/fit_7674.jpg',
 //]);
 
-# https://stackoverflow.com/questions/11333591/find-similar-images-in-pure-php-mysql
-# https://github.com/jenssegers/imagehash
-# https://www.hackerfactor.com/blog/?/archives/432-Looks-Like-It.html
-
-# Generate an image hash
+/**
+ * Hash Notes
+ *
+ * References
+ * - https://stackoverflow.com/questions/11333591/find-similar-images-in-pure-php-mysql
+ * - https://github.com/jenssegers/imagehash
+ * - https://www.hackerfactor.com/blog/?/archives/432-Looks-Like-It.html
+ */
 
 /*
  * Generate a Perceptual Image Hash
  */
 $pimage->out->hash([
-	'save_path' => $img_dir . '/hash_test_person.jpg',
-//	'save_path' => $img_dir . '/hash_test_unflipped.jpg',
+	'save_path' => $img_dir . '/hash_test.jpg',
 ]);
 
 $pimage->save->image();                               # Save the images
